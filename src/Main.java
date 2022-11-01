265ae9d4a54a185a66112f60e9b2a745a8ad048b
b.Mashrapov <b_mashrapov@mail.ru>
2022-11-01 18:52:35 +0100

    Задание 4-9

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,41 @@ public class Main {
             System.out.println("Итерация цикла " + a);
         }
         System.out.println("Задание 3");
-        for (int b = 0; b < 17; b= b +2 ){
+        for (int b = 0; b < 17; b = b + 2) {
             System.out.println("Чётные числа в отрезке от 0 до 17 это: " + b);
         }
         System.out.println("Задание 4");
-        for (int c = 10; c >= -10; c--){
+        for (int c = 10; c >= -10; c--) {
             System.out.println("Числа убывают " + c);
+        }
+        System.out.println("Задание 5");
+        for (int year = 1904; year < 2096; year = year + 4) {
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                System.out.println(year + " год является вискосным");
+        }
+        System.out.println("Задание 6");
+        for (int h = 7; h <= 98; h = h + 7) {
+            System.out.println(h);
+        }
+        System.out.println("Задание 7");
+        for (int d = 1; d <= 512; d = d * 2) {
+            System.out.println(d);
+        }
+        System.out.println("Задание 8");
+        int saving = 29000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total = total + total / 100;
+            total = total + saving;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+        System.out.println("Задание 9");
+        int saving1 = 29000;
+        int total1 = 0;
+        for (int i = 1; i <= 12; i++) {
+            total1 = total1 + total1 / 100;
+            total1 = total1 + saving1;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total1 + " рублей");
         }
     }
 }
